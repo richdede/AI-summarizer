0555e3ed245d9b9edd26f7af422bb5f1ac9ba995
richdede <deeprince2002@gmail.com>
2023-11-29 05:10:28 -0800

    updated github address

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/", "_blank")
+            window.open("https://github.com/richdede/AI-summarizer", "_blank")
           }
           className='black_btn'
         >
